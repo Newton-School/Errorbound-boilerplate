a42eb94a9765489f423c106a27dd8e27bb49fe63
trojan1771 <76389598+trojan1771@users.noreply.github.com>
2024-05-01 12:34:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import React from 'react'
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { error: null, errorInfo: null }
   }
 
   componentDidCatch(error, errorInfo) {
-    // Catch errors in any components below and re-render with error message
-    this.setState({
-      error: error,
-      errorInfo: errorInfo,
-    })
-    // You can also log error messages to an error reporting service here
   }
 
   render() {
-    if (this.state.errorInfo) {
+    if () {
       // Error path
       return (
         <div>
@@ -36,22 +29,19 @@ class ErrorBoundary extends React.Component {
 class BuggyCounter extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { counter: 0 }
-    this.handleClick = this.handleClick.bind(this)
+
   }
 
   handleClick() {
-    this.setState(({ counter }) => ({
-      counter: counter + 1,
-    }))
+   
   }
 
   render() {
-    if (this.state.counter === 5) {
+    if () {
       // Simulate a JS error
       throw new Error('I crashed!')
     }
-    return <h1 onClick={this.handleClick}>{this.state.counter}</h1>
+    return ;
   }
 }
 
@@ -59,25 +49,25 @@ function App() {
   return (
     <div>
       <hr />
-      <ErrorBoundary>
+     
         <p>
           These two counters are inside the same error boundary. If one crashes,
           the error boundary will replace both of them.
         </p>
         <BuggyCounter />
         <BuggyCounter />
-      </ErrorBoundary>
+      
       <hr />
       <p>
         These two counters are each inside of their own error boundary. So if
         one crashes, the other is not affected.
       </p>
-      <ErrorBoundary>
+    
         <BuggyCounter />
-      </ErrorBoundary>
-      <ErrorBoundary>
+
+ 
         <BuggyCounter />
-      </ErrorBoundary>
+
     </div>
   )
 }
